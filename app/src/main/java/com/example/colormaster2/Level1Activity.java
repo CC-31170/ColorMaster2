cee1ed359ce0fad0c6b61cd18142ef5aae9565d9
Yunju <2590505449@qq.com>
2024-11-17 19:17:59 +0800

    add level 1 layout

--- app/src/main/java/com/example/colormaster2/Level1Activity.java
+++ app/src/main/java/com/example/colormaster2/Level1Activity.java
@@ -1,32 +1,20 @@
 package com.example.colormaster2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LevelChooseActivity extends AppCompatActivity {
+public class Level1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_level_choose);
-
-        Button LevelBtn1 = findViewById(R.id.LevelBtn1);
-        LevelBtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LevelChooseActivity.this, Level1Activity.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_level1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
