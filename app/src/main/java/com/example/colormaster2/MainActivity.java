fb055a939617a287cfda0f2161d91f358c238e97
Liu Yunju <liuyunju2002@163.com>
2024-12-05 18:56:37 +0800

    version 1.0
    EE5415 individual project

--- app/src/main/java/com/example/colormaster2/MainActivity.java
+++ app/src/main/java/com/example/colormaster2/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.colormaster2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button StartBtn = findViewById(R.id.Start);
+        ImageButton InfoBtn = findViewById(R.id.InfoBtn);
+
+        InfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("About ColorMaster2");
+                builder.setMessage("This is a color match game\nMix proper " +
+                                "color to get high marks\nThe picture disappears after 3s");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    { }
+                });
+                builder.create();
+                builder.show();
+
+            }
+        });
         StartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
